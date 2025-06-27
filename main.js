d7ce6044d82c529b22f490b80ebdaa34e90dff84
rioootsuka <rvonzin0727@gmail.com>
2025-06-27 22:53:35 +0900

    最初のコミット

---
+++ main.js
@@ -0,0 +1,33 @@
+// エラーが出たら教えてくれるコード
+'use strict';
+
+// function createRandomArray(count) {
+//   const array = [];
+//   for (var i = 0; i < count; i++){
+//     const randomNumber = Math.floor(Math.random() * 100);
+//     array.push(randomNumber);
+//   }
+//   return array
+// }
+// console.log(createRandomArray(5));
+
+// //============================================================
+
+// function multiplyArray(array, num){
+//   return array.map(item => item * num)
+//   }
+
+
+
+  
+
+
+// //============================================================
+
+// var array = createRandomArray(10); 
+// multiplyArray(array, 5);          
+
+// for(var i = 0; i < array.length; i++){
+//     console.log(array[i]);
+// }
+
